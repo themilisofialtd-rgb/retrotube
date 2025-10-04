5c3b39f6cf2d5829737e63929d35b56b0b00bee1
themilisofialtd-rgb <themilisofialtd@gmail.com>
2025-10-04 12:53:03 +0200

    Add files via upload

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wp-script/retrotube',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2f7e2f72024c84a3fce816461ee4c25abf22edd1',
+        'type' => 'wordpress-theme',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'wp-script/retrotube' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2f7e2f72024c84a3fce816461ee4c25abf22edd1',
+            'type' => 'wordpress-theme',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
